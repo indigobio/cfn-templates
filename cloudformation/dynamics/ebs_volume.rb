1b098668893ff579310d9d18baf2dd9afa8a46d0
Greg Swallow <gswallow@gmail.com>
2015-02-13 18:43:10 -0500

    Add EBS volumes and attachments.

--- cloudformation/dynamics/ebs_volume.rb
+++ cloudformation/dynamics/ebs_volume.rb
@@ -1,5 +1,12 @@
-SparkleFormation.dynamic(:ebs_volume) do |_name, _config|
+SparkleFormation.dynamic(:ebs_volume) do |_name, _config = { :snapshot_id => 'none' }|
   # _config[:instance] must be supplied.
+
+  instance_eval(
+  "conditions do
+    #{_name}_is_from_snapshot not!(equals!(ref!(:#{_name}_ebs_snapshot_id), 'none'))
+    #{_name}_is_io1 equals!(ref!(:#{_name}_ebs_volume_type), 'io1')
+  end"
+  )
 
   parameters("#{_name}_ebs_volume_size".to_sym) do
     type 'Number'
@@ -24,9 +31,16 @@ SparkleFormation.dynamic(:ebs_volume) do |_name, _config|
 
   parameters("#{_name}_ebs_snapshot_id".to_sym) do
     type 'String'
-    allowed_pattern "snap-[\da-f]{8}|none"
+    allowed_pattern "snap-[\\da-f]{8}|none"
     default _config[:snapshot_id] || 'none'
     description 'Snapshot from which to create EBS volume'
+  end
+
+  parameters("#{_name}_ebs_device_label") do
+    type 'String'
+    allowed_pattern "(\\/dev\\/sd|xvd)[f-z]+"
+    default _config[:device] || '/dev/sdi'
+    description 'The device label for the EBS volume (/dev/sdX)'
   end
 
   resources("#{_name}_ebs_volume".to_sym) do
@@ -34,9 +48,21 @@ SparkleFormation.dynamic(:ebs_volume) do |_name, _config|
     properties do
       availability_zone attr!(_config[:instance], :availability_zone)
       volume_type ref!("#{_name}_ebs_volume_type".to_sym)
-      snapshot_id if!(equals!(ref!("#{_name}_ebs_snapshot_id".to_sym), 'none'), ref!('AWS::NoValue'), ref!("#{_name}_ebs_snapshot_id".to_sym))
-      iops if!(equals!(ref!("#{_name}_ebs_volume_type".to_sym), 'io1'), ref!("#{_name}_ebs_provisioned_iops".to_sym), ref!('AWS::NoValue'))
+      snapshot_id if!("#{_name}_is_from_snapshot".to_sym, ref!("#{_name}_ebs_snapshot_id".to_sym), ref!('AWS::NoValue'))
+      iops if!("#{_name}_is_io1".to_sym, ref!("#{_name}_ebs_provisioned_iops".to_sym), ref!('AWS::NoValue'))
       size ref!("#{_name}_ebs_volume_size".to_sym)
+    end
+  end
+
+  # I feel like adding a volume attachment here is "mixing concerns" but then again
+  # what's the point of re-importing instance ID, etc. to another dynamic?
+
+  resources("#{_name}_ebs_volume_attachment".to_sym) do
+    type 'AWS::EC2::VolumeAttachment'
+    properties do
+      device ref!("#{_name}_ebs_device_label".to_sym)
+      instance_id ref!(_config[:instance])
+      volume_id ref!("#{_name}_ebs_volume".to_sym)
     end
   end
 end
