c4fa5b0a2cf7c648d29f5371bb8ab230e56c142d
Greg Swallow <gswallow@gmail.com>
2015-02-18 09:38:27 -0500

    http://www.nuvolecomputing.com/2012/07/02/chef-node-de-registration-for-autoscaling-groups/

--- cloudformation/components/deregistration.rb
+++ cloudformation/components/deregistration.rb
@@ -1,20 +1,54 @@
 SparkleFormation.build do
   set!('AWSTemplateFormatVersion', '2010-09-09')
 
+  conditions.set!(
+    :dereg_topic_has_name,
+    not!(equals!(ref!(:dereg_topic_name), 'none'))
+  )
+
+  parameters(:dereg_queue_name) do
+    type 'String'
+    allowed_pattern "[\\x20-\\x7E]*"
+    constraint_description 'can only contain ASCII characters'
+    default 'instance_dereg_notifications'
+  end
+
+  parameters(:dereg_error_queue_name) do
+    type 'String'
+    allowed_pattern "[\\x20-\\x7E]*"
+    constraint_description 'can only contain ASCII characters'
+    default 'instance_dereg_errors'
+  end
+
+  parameters(:dereg_topic_name) do
+    type 'String'
+    allowed_pattern "[\\x20-\\x7E]*"
+    constraint_description 'can only contain ASCII characters'
+    default 'none'
+  end
+
   resources(:dereg_queue) do
-    type 'AWS::SNS::Queue'
+    type 'AWS::SQS::Queue'
+    properties do
+      queue_name ref!(:dereg_queue_name)
+    end
   end
 
   resources(:dereg_error_queue) do
-    type 'AWS::SNS::Queue'
+    type 'AWS::SQS::Queue'
+    properties do
+      queue_name ref!(:dereg_error_queue_name)
+    end
   end
 
   resources(:dereg_topic) do
     type 'AWS::SNS::Topic'
     properties do
+      topic_name if!(:dereg_topic_has_name, ref!(:dereg_topic_name), no_value!)
       subscription _array(
         -> {
           endpoint attr!(:dereg_queue, :arn)
+          protocol 'sqs'
         }
       )
     end
@@ -24,19 +58,26 @@ SparkleFormation.build do
     type 'AWS::SQS::QueuePolicy'
     properties do
       policy_document do
-        id :dereg_queue_policy
-        statement do
-          sid 'Allow-SendMessage-To-Queue-From-SNS-Topic'
-          effect 'Allow'
-          principal.set!("AWS", "*")
-          action _array('sqs:SendMessage')
-          resource '*'
-          condition do
-            arn_equals.set!('aws:SourceARN', ref!(:dereg_topic))
-          end
-        end
+        id 'DeregQueuePolicy'
+        statement _array(
+          -> {
+            sid 'Allow-SendMessage-To-Queue-From-SNS-Topic'
+            effect 'Allow'
+            principal.set!("AWS", "*")
+            action _array('sqs:SendMessage')
+            resource '*'
+            condition do
+              arn_equals.set!('aws:SourceARN', ref!(:dereg_topic))
+            end
+          }
+        )
       end
+      queues _array(ref!(:dereg_queue))
     end
-    queues _array(ref!(:dereg_queue))
+  end
+
+  outputs(:dereg_topic) do
+    description "SNS Topic for Instance Deregistrations"
+    value ref!(:dereg_topic)
   end
 end
