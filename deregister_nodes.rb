4f31d1fdf498b58ed3ed61a35c2501e8a42b8f58
Greg Swallow <gswallow@gmail.com>
2015-02-18 11:34:05 -0500

    Delete nodes by name rather than by instance id.

--- deregister_nodes.rb
+++ deregister_nodes.rb
@@ -1,19 +1,26 @@
+#!/usr/bin/env ruby
 require 'rubygems'
-require 'aws-sdk'
+require 'aws-sdk' # Must be aws-sdk v1
 require 'json'
 require 'time'
 require 'chef'
 
 asg_queue_url = ENV['ASG_DEREG_QUEUE_URL']
 asg_error_queue_url = ENV['ASG_DEREG_ERROR_QUEUE_URL']
-topic_arn= ENV['WARNING_TOPIC']
+
+# Use a different topic than the one that the ASG_DEREG_QUEUE_URL subscribes to.
+warning_topic_arn = ENV['WARNING_TOPIC']
+
 sqs = AWS::SQS.new(:access_key_id => ENV['ADMINPROC_USER'],
                    :secret_access_key => ENV['ADMINPROC_PASS'],
                    :sqs_endpoint => asg_queue_url)
 sns = AWS::SNS.new(:access_key_id => ENV['RELEASE_USER'],
                    :secret_access_key => ENV['RELEASE_PASS'])
+
 Chef::Config.from_file(ENV['HOME'] + '/.chef/knife.rb')
 rest = Chef::REST.new(Chef::Config[:chef_server_url])
+query = Chef::Search::Query.new
+name = String.new
 
 sqs.queues[asg_queue_url].poll do |m|
   sns_msg = m.as_sns_message
@@ -24,24 +31,25 @@ sqs.queues[asg_queue_url].poll do |m|
       time = Time.now.utc.iso8601
       # here we assume that the ec2 instance id = node name
       iid = body["EC2InstanceId"]
+      puts "identifying node by instance id " + iid + "\n"
+      name = query.search(:node, "ec2_instance_id:#{iid}").first[0].name
 
-      puts "deleting node " + iid + "\n"
-      del_node = rest.delete_rest("/nodes/" + iid)
+      puts "deleting node " + name + "\n"
+      del_node = rest.delete_rest("/nodes/" + name)
 
-      puts "deleting client " + iid + "\n"
-      del_client = rest.delete_rest("/clients/" + iid)
+      puts "deleting client " + name + "\n"
+      del_client = rest.delete_rest("/clients/" + name)
 
     elsif event.include? "autoscaling:TEST_NOTIFICATION"
       m.delete
     end
 
-      # on failure:
   rescue
-    msg = "There was a problem deregistering instance #{iid}.\n" + $!.to_s + "\n" + body.to_a.sort.join("\n")
+    msg = "There was a problem deregistering instance #{iid} (#{name}).\n" + $!.to_s + "\n" + body.to_a.sort.join("\n")
     puts msg    # should go to STDERR
 
     # send alert to staff
-    topic = sns.topics[topic_arn]
+    topic = sns.topics[warning_topic_arn]
     topic.publish(msg)
 
     # put the message in DeregErrorQueue
