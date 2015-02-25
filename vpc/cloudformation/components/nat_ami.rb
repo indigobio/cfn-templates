e49885284fa5ad8d06dc9d14354158ad7b0ec993
Greg Swallow <gswallow@gmail.com>
2015-02-24 21:10:42 -0500

    The NAT AMI from Amazon has its own user data.

--- vpc/cloudformation/components/nat_ami.rb
+++ vpc/cloudformation/components/nat_ami.rb
@@ -1,7 +1,9 @@
 SparkleFormation.build do
   mappings.region_to_nat_ami do
-    set!('us-east-1'._no_hump, :ami => 'ami-184dc970') # amzn-ami-vpc-nat-hvm-2014.09.1.x86_64-gp2
-    set!('us-west-1'._no_hump, :ami => 'ami-a98396ec')
-    set!('us-west-2'._no_hump, :ami => 'ami-290f4119')
+    set!('us-east-1'._no_hump,    :ami => 'ami-146e2a7c') # amzn-ami-hvm-2014.09.2.x86_64-ebs
+    set!('us-west-1'._no_hump,    :ami => 'ami-42908907')
+    set!('us-west-2'._no_hump,    :ami => 'ami-dfc39aef')
+    set!('eu-west-1'._no_hump,    :ami => 'ami-9d23aeea')
+    set!('eu-central-1'._no_hump, :ami => 'ami-04003319')
   end
 end
