80f7c1b5cd70ac9556d6ac7a260dd15401d3fd5e
Greg Swallow <gswallow@gmail.com>
2015-02-24 22:46:42 -0500

    Update comment.

--- vpc/cloudformation/components/nat_ami.rb
+++ vpc/cloudformation/components/nat_ami.rb
@@ -1,6 +1,6 @@
 SparkleFormation.build do
   mappings.region_to_nat_ami do
-    set!('us-east-1'._no_hump,    :ami => 'ami-146e2a7c') # amzn-ami-hvm-2014.09.2.x86_64-ebs
+    set!('us-east-1'._no_hump,    :ami => 'ami-146e2a7c') # amzn-ami-hvm-2014.09.2.x86_64-ebs (NOT the nat ami)
     set!('us-west-1'._no_hump,    :ami => 'ami-42908907')
     set!('us-west-2'._no_hump,    :ami => 'ami-dfc39aef')
     set!('eu-west-1'._no_hump,    :ami => 'ami-9d23aeea')
