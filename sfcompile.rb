503b12a354709a9d99d73d4ba1c3292d46c10df0
Greg Swallow <gswallow@gmail.com>
2015-02-27 13:37:11 -0500

    fix merged sfcompile tool

--- sfcompile.rb
+++ sfcompile.rb
@@ -5,19 +5,18 @@ require 'json'
 require 'fog'
 
 cf = Fog::AWS::CloudFormation.new(
-    :aws_access_key_id     => ENV['AWS_ACCESS_KEY'],
-    :aws_secret_access_key => ENV['AWS_SECRET_KEY']
+  :aws_access_key_id     => ENV['AWS_ACCESS_KEY'],
+  :aws_secret_access_key => ENV['AWS_SECRET_KEY']
 )
 
 template = JSON.pretty_generate(SparkleFormation.compile(ARGV[0]))
+File.open(ARGV[1], 'w').puts template
 
 begin
-    response = cf.validate_template( "TemplateBody" => template)
+  response = cf.validate_template( "TemplateBody" => template)
 rescue Excon::Errors::BadRequest => e
-    puts "Validation error: 400 Bad request"
-      puts e.inspect
+  puts "Validation error: 400 Bad request"
+#  puts e.inspect
 else
-    puts "Template validated successfully."
+  puts "Template validated successfully."
 end
-
-File.open(ARGV[1], 'w').puts template
