db158193ed671b93135840d40222ef2dafdb4f66
Greg Swallow <gswallow@gmail.com>
2015-02-27 13:05:30 -0500

    Combine sfcompile.rb and validate.rb.
    Plus some additional pruning.

--- sfcompile.rb
+++ sfcompile.rb
@@ -2,7 +2,22 @@
 
 require 'sparkle_formation'
 require 'json'
+require 'fog'
 
-puts JSON.pretty_generate(
-  SparkleFormation.compile(ARGV[0])
+cf = Fog::AWS::CloudFormation.new(
+    :aws_access_key_id     => ENV['AWS_ACCESS_KEY'],
+    :aws_secret_access_key => ENV['AWS_SECRET_KEY']
 )
+
+template = JSON.pretty_generate(SparkleFormation.compile(ARGV[0]))
+
+begin
+    response = cf.validate_template( "TemplateBody" => template)
+rescue Excon::Errors::BadRequest => e
+    puts "Validation error: 400 Bad request"
+      puts e.inspect
+else
+    puts "Template validated successfully."
+end
+
+File.open(ARGV[1], 'w').puts template
