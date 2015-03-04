37e4b7f145e56130d6bb8496b16c56b11756b95a
Greg Swallow <gswallow@gmail.com>
2015-03-04 12:29:56 -0500

    Add a "validate-only" function.

--- sfcompile.rb
+++ sfcompile.rb
@@ -1,16 +1,25 @@
 #!/usr/bin/env ruby
 
+require 'trollop'
 require 'sparkle_formation'
 require 'json'
 require 'fog'
+
+opts = Trollop::options do
+  opt :validate, 'Validate the template only.'
+end
 
 cf = Fog::AWS::CloudFormation.new(
   :aws_access_key_id     => ENV['AWS_ACCESS_KEY'],
   :aws_secret_access_key => ENV['AWS_SECRET_KEY']
 )
 
-template = JSON.pretty_generate(SparkleFormation.compile(ARGV[0]))
-File.open(ARGV[1], 'w').puts template
+if opts[:validate]
+  template = File.open(ARGV[0], 'r').read
+else
+  template = JSON.pretty_generate(SparkleFormation.compile(ARGV[0]))
+  File.open(ARGV[1], 'w').puts template
+end
 
 begin
   response = cf.validate_template( "TemplateBody" => template)
