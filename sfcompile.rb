bc00cc71077b8a4084920f9af64b4ba70e4f9a63
Greg Swallow <gswallow@gmail.com>
2015-05-12 15:00:36 -0400

    Small refactor.

--- sfcompile.rb
+++ sfcompile.rb
@@ -14,18 +14,16 @@ cf = Fog::AWS::CloudFormation.new(
   :aws_secret_access_key => ENV['AWS_SECRET_KEY']
 )
 
-if opts[:validate]
-  template = File.open(ARGV[0], 'r').read
-else
-  template = JSON.pretty_generate(SparkleFormation.compile(ARGV[0]))
+template = JSON.pretty_generate(SparkleFormation.compile(ARGV[0]))
+unless opts[:validate]
   File.open(ARGV[1], 'w').puts template
 end
 
 begin
-  response = cf.validate_template( "TemplateBody" => template)
+  cf.validate_template( "TemplateBody" => template)
 rescue Excon::Errors::BadRequest => e
-  puts "Validation error: 400 Bad request"
-#  puts e.inspect
-else
-  puts "Template validated successfully."
+  puts "Validation error: 400 Bad request."
+  exit 1
 end
+
+puts "Template validated successfully."
