44238ffd684fe77b163714102f7c57a123e663ed
Greg Swallow <gswallow@gmail.com>
2015-05-20 15:52:38 -0400

    Clean up documentation and set auto scaling group size.

--- stacks/cloudformation/templates/quartermasters.rb
+++ stacks/cloudformation/templates/quartermasters.rb
@@ -43,13 +43,13 @@ topic = topics.find { |e| e =~ /#{ENV['notification_topic']}/ }
 SparkleFormation.new('quartermaster').load(:precise_ami, :ssh_key_pair, :chef_validator_key_bucket).overrides do
   set!('AWSTemplateFormatVersion', '2010-09-09')
   description <<EOF
-Creates an auto scaling group containing quartermaster instances.  Each instance is given an IAM instance profile,
-which allows the instance to get objects from the Chef Validator Key Bucket.
+Creates an auto scaling group containing quartermaster instances.  Each instance is given an IAM instance profile, which allows the instance to get validator keys and encrypted
+data bag secrets from the Chef validator key bucket.
 
-Depends on the fileserver, rabbitmq, and databases templates.
+Launching this stack requires a VPC with a matching environment tag.  Chef will not work unless databases and file servers are up.
 EOF
 
   dynamic!(:iam_instance_profile, 'default')
   dynamic!(:launch_config_chef_bootstrap, 'quartermaster', :instance_type => 'm3.medium', :create_ebs_volumes => false, :security_groups => sgs, :chef_run_list => 'role[base],role[quartermaster]')
-  dynamic!(:auto_scaling_group, 'quartermaster', :launch_config => :quartermaster_launch_config, :subnets => subnets, :notification_topic => topic)
+  dynamic!(:auto_scaling_group, 'quartermaster', :launch_config => :quartermaster_launch_config, :min_size => 1, :desired_capacity => 2, :max_size => 2, :subnets => subnets, :notification_topic => topic)
 end
