30394f2dee107a82e4e321d29d214d81d6ba41f6
Greg Swallow <gswallow@gmail.com>
2015-05-21 13:51:56 -0400

    Set a default ssh key

--- stacks/cloudformation/components/ssh_key_pair.rb
+++ stacks/cloudformation/components/ssh_key_pair.rb
@@ -1,6 +1,9 @@
+ENV['org'] ||= 'indigo'
+
 SparkleFormation.build do
   parameters(:ssh_key_pair) do
     description 'Amazon EC2 key pair'
     type 'AWS::EC2::KeyPair::KeyName'
+    default "#{ENV['org']}-bootstrap"
   end
 end
