4fa3caef0e72912ea08505a3327cb1cbce1e067b
Greg Swallow <gswallow@gmail.com>
2015-05-28 22:11:39 -0400

    Fix typo in route53 policy statements.

--- stacks/cloudformation/registry/modify_route53.rb
+++ stacks/cloudformation/registry/modify_route53.rb
@@ -3,7 +3,7 @@ SparkleFormation::Registry.register(:modify_route53) do
   { 'Action' => %w(route53:ChangeResourceRecordSets
                    route53:ChangeTagsForResource
                    route53:Get*
-                   route53:ListH*),
+                   route53:List*),
     'Resource' => %w( * ),
     'Effect' => 'Allow'
   }
