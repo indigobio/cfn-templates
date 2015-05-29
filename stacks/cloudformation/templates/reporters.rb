b631bec6f57f98cfc34ca716feac0e838928e08b
Greg Swallow <gswallow@gmail.com>
2015-05-29 10:58:04 -0400

    Split reporters and daemons.

--- stacks/cloudformation/templates/reporters.rb
+++ stacks/cloudformation/templates/reporters.rb
@@ -46,10 +46,10 @@ topic = topics.find { |e| e =~ /#{ENV['notification_topic']}/ }
 
 # Build the template.
 
-SparkleFormation.new('daemons').load(:precise_ami, :ssh_key_pair, :chef_validator_key_bucket).overrides do
+SparkleFormation.new('reporters').load(:precise_ami, :ssh_key_pair, :chef_validator_key_bucket).overrides do
   set!('AWSTemplateFormatVersion', '2010-09-09')
   description <<EOF
-Creates auto scaling groups for backend daemons including control batch sets, purgery, etc.  Each instance is
+Creates auto scaling groups for backend reporters including report servers.  Each instance is
 given an IAM instance profile, which allows the instance to get objects from the Chef Validator Key Bucket.
 
 Run this template while running the compute and webserver templates.  Depends on the rabbitmq
@@ -58,12 +58,9 @@ EOF
 
   dynamic!(:iam_instance_profile, 'default')
 
-  dynamic!(:launch_config_chef_bootstrap, 'purgery', :instance_type => 't2.small', :create_ebs_volumes => false, :security_groups => sgs, :chef_run_list => 'role[base],role[purgery]')
-  dynamic!(:auto_scaling_group, 'purgery', :launch_config => :purgery_launch_config, :subnets => subnets, :notification_topic => topic)
+  dynamic!(:launch_config_chef_bootstrap, 'reporter', :instance_type => 'm3.medium', :create_ebs_volumes => false, :security_groups => sgs, :chef_run_list => 'role[base],role[reporter],role[reportcatcher]')
+  dynamic!(:auto_scaling_group, 'reporter', :launch_config => :reporter_launch_config, :subnets => subnets, :notification_topic => topic)
 
-  dynamic!(:launch_config_chef_bootstrap, 'cbs', :instance_type => 't2.small', :create_ebs_volumes => false, :security_groups => sgs, :chef_run_list => 'role[base],role[cbs_reporter],role[cbs_reportcatcher],role[cbs_summaries]')
-  dynamic!(:auto_scaling_group, 'cbs', :launch_config => :cbs_launch_config, :subnets => subnets, :notification_topic => topic)
-
-  dynamic!(:launch_config_chef_bootstrap, 'correlator', :instance_type => 't2.small', :create_ebs_volumes => false, :security_groups => sgs, :chef_run_list => 'role[base],role[correlator]')
-  dynamic!(:auto_scaling_group, 'correlator', :launch_config => :cbs_launch_config, :subnets => subnets, :notification_topic => topic)
+  dynamic!(:launch_config_chef_bootstrap, 'customreports', :instance_type => 't2.small', :create_ebs_volumes => false, :security_groups => sgs, :chef_run_list => 'role[base],role[custom_reports]')
+  dynamic!(:auto_scaling_group, 'customreports', :launch_config => :customreports_launch_config, :subnets => subnets, :notification_topic => topic)
 end
