b7f8470a625a013a625e360bce8f7ee4dabd7d3c
Greg Swallow <gswallow@gmail.com>
2015-08-06 15:10:13 -0400

    Don't base64 encode.

--- stacks/cloudformation/registry/register_with_elb.rb
+++ stacks/cloudformation/registry/register_with_elb.rb
@@ -1,5 +1,5 @@
 SparkleFormation::Registry.register(:register_with_elb) do
-  base64! <<-EOF
+  <<-EOF
 curl https://s3.amazonaws.com/aws-cli/awscli-bundle.zip -o /tmp/awscli-bundle.zip
 pushd /tmp
 unzip awscli-bundle.zip
