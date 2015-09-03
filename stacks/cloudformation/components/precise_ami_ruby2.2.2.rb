d7c7a2142ba5c839bc68f38da4b85b9a580dcca2
Greg Swallow <gswallow@gmail.com>
2015-09-03 18:50:28 -0400

    New AMI that has ruby 2.2.2 on it.

---
+++ stacks/cloudformation/components/precise_ami_ruby2.2.2.rb
@@ -0,0 +1,10 @@
+SparkleFormation.build do
+  # For simplicity's sake we'll just use hvm:ebs-ssd AMIs for now. Currently based on 12.04.5 Release 20150728
+  mappings(:region_to_precise_ami) do
+    set!('us-east-1'._no_hump, :ami => 'ami-3de38358') # ami-a7558fcc
+    set!('us-west-1'._no_hump, :ami => 'ami-ab6f94ef') # ami-039b6747
+    set!('us-west-2'._no_hump, :ami => 'ami-c78291f7') # ami-2d6e601d
+    set!('eu-west-1'._no_hump, :ami => 'ami-42722635')
+    set!('eu-central-1'._no_hump, :ami => 'ami-38b8bd25')
+  end
+end
