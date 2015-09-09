769a789a1efa1fff3a126cdaf7c9908d334be26c
Greg Swallow <gswallow@gmail.com>
2015-09-08 21:38:04 -0400

    Update ami ids.

--- stacks/cloudformation/components/precise_ami.rb
+++ stacks/cloudformation/components/precise_ami.rb
@@ -1,9 +1,9 @@
 SparkleFormation.build do
   # For simplicity's sake we'll just use hvm:ebs-ssd AMIs for now. Currently based on 12.04.5 Release 20150728
   mappings(:region_to_precise_ami) do
-    set!('us-east-1'._no_hump, :ami => 'ami-9f5feaf4') # ami-a7558fcc
-    set!('us-west-1'._no_hump, :ami => 'ami-c17d8385') # ami-039b6747
-    set!('us-west-2'._no_hump, :ami => 'ami-e70f1ad7') # ami-2d6e601d
+    set!('us-east-1'._no_hump, :ami => 'ami-ddd2bdb8') # ami-a7558fcc
+    set!('us-west-1'._no_hump, :ami => 'ami-dd35cf99') # ami-039b6747
+    set!('us-west-2'._no_hump, :ami => 'ami-a30c1193') # ami-2d6e601d
     set!('eu-west-1'._no_hump, :ami => 'ami-42722635')
     set!('eu-central-1'._no_hump, :ami => 'ami-38b8bd25')
   end
