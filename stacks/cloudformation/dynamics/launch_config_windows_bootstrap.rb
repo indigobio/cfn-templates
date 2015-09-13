1f981d39080a344e5d2290d24e1d0e755e136444
Greg Swallow <gswallow@gmail.com>
2015-09-13 16:23:24 -0400

    Add m4 instances.

--- stacks/cloudformation/dynamics/launch_config_windows_bootstrap.rb
+++ stacks/cloudformation/dynamics/launch_config_windows_bootstrap.rb
@@ -32,6 +32,7 @@ SparkleFormation.dynamic(:launch_config_windows_bootstrap) do |_name, _config = 
     type 'String'
     allowed_values %w(t2.micro  t2.small   t2.medium
                       m3.medium m3.large   m3.xlarge  m3.2xlarge
+                      m4.large  m4.xlarge  m4.2xlarge m4.4xlarge m4.10xlarge
                       c3.large  c3.xlarge  c3.2xlarge c3.4xlarge c3.8xlarge
                       c4.large  c4.xlarge  c4.2xlarge c4.4xlarge c4.8xlarge
                       r3.large  r3.xlarge  r3.2xlarge r3.4xlarge r3.8xlarge
