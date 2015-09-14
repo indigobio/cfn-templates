0943228866b1559ac37bf387cbbd9f9e842be1ea
Greg Swallow <gswallow@gmail.com>
2015-09-13 23:07:00 -0400

    Fix volumes.

--- stacks/cloudformation/templates/fileserver.rb
+++ stacks/cloudformation/templates/fileserver.rb
@@ -1,3 +1,5 @@
+ENV['volume_count'] ||= '2'
+ENV['volume_size']  ||= '10'
 ENV['net_type'] ||= 'Private'
 ENV['sg']       ||= 'private_sg'
 ENV['run_list'] ||= 'role[base],role[file_server]'
@@ -22,7 +24,7 @@ EOF
 
   dynamic!(:iam_instance_profile, 'default')
 
-  dynamic!(:launch_config_chef_bootstrap, 'fileserver', :instance_type => 't2.small', :create_ebs_volumes => true, :volume_count => 2, :volume_size => 10, :security_groups => lookup.get_security_groups(vpc), :chef_run_list => ENV['run_list'])
+  dynamic!(:launch_config_chef_bootstrap, 'fileserver', :instance_type => 't2.small', :create_ebs_volumes => true, :volume_count => ENV['volume_count'], :volume_size => ENV['volume_size'], :security_groups => lookup.get_security_groups(vpc), :chef_run_list => ENV['run_list'])
   dynamic!(:auto_scaling_group, 'fileserver', :launch_config => :fileserver_launch_config, :subnets => lookup.get_subnets(vpc), :notification_topic => lookup.get_notification_topic)
 
 end
