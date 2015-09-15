70696e4398836c48b16b4ccb3881afbf71eeb729
Greg Swallow <gswallow@gmail.com>
2015-09-14 20:45:49 -0400

    (facepalm)

--- stacks/cloudformation/components/precise_ruby22_ami.rb
+++ stacks/cloudformation/components/precise_ruby22_ami.rb
@@ -1,9 +1,9 @@
 SparkleFormation.build do
   # For simplicity's sake we'll just use hvm:ebs-ssd AMIs for now. Currently based on 12.04.5 Release 20150728
   mappings(:region_to_precise_ami) do
-    set!('us-east-1'._no_hump, :ami => 'ami-d335cf97') # ami-a7558fcc
-    set!('us-west-1'._no_hump, :ami => 'ami-a10c1191') # ami-039b6747
-    set!('us-west-2'._no_hump, :ami => 'ami-dbd2bdbe') # ami-2d6e601d
+    set!('us-east-1'._no_hump, :ami => 'ami-dbd2bdbe') # ami-a7558fcc
+    set!('us-west-1'._no_hump, :ami => 'ami-d335cf97') # ami-039b6747
+    set!('us-west-2'._no_hump, :ami => 'ami-a10c1191') # ami-2d6e601d
     set!('eu-west-1'._no_hump, :ami => 'ami-42722635')
     set!('eu-central-1'._no_hump, :ami => 'ami-38b8bd25')
   end
