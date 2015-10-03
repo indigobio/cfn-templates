9b5133efd4e3b84e9d4263e466bc0bca194a8c18
Greg Swallow <gswallow@gmail.com>
2015-10-03 14:54:37 -0400

    Still testing passing a list into a method.

--- utils/jenkins/workflow-qa1.groovy
+++ utils/jenkins/workflow-qa1.groovy
@@ -1,4 +1,4 @@
-def build_step(step, params) {
+def build_step(step, List params = []) {
   def workflow_env = 'qa1'
   build job: step,
   parameters: [
@@ -18,13 +18,21 @@ def build_step(step, params) {
   ]
 }
 
-build_step('100-launch-vpc', *[[$class: "StringParameterValue", name: "allow_ssh", value: '207.250.246.0/24']])
-parallel first: {
+build_step('100-launch-vpc', [$class: 'StringParameterValue', name: "allow_ssh", value: '207.250.246.0/24'])
+
+//parallel first: {
   build_step('110-launch-nexus-rds', [[$class: 'TextParameterValue', name: 'instance_type', value: 'db.t2.micro']])
-}, second: {
-  try {
-    build_step ('210-launch-vpn', [[$class: 'TextParameterValue', name: 'instance_type', value: 't2.micro']])
-  } catch (Exception e) {
-    echo 'Whoops.  Launching the vpn failed: ' + e
-  }
-}
+//}, second: {
+//  try {
+//    build_step ('210-launch-vpn', [[$class: 'TextParameterValue', name: 'instance_type', value: 't2.micro']])
+//  } catch (Exception e) {
+//    echo 'Whoops.  Launching the vpn failed: ' + e
+//  }
+//}
+
+//parallel first: {
+//  build_step('300-launch-couchbase', [[$class: 'StringParameterValue', name: 'instance_type', value: 't2.micro']])
+//},
+//second: {
+  build_step('320-launch-fileserver', [[$class: 'StringParameterValue', name: 'instance_type', value: 't2.micro'], [$class: 'StringParameterValue', name: 'volume_size', value: '20'], [$class: 'StringParameterValue', name: 'volume_count', value: '2']])
+//}
