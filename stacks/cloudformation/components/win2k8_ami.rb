c8c0f2e9462f516d05a0a3ce74fbab425353df17
Greg Swallow <gswallow@gmail.com>
2015-10-04 19:46:14 -0400

    Catch up to windows 2008.

--- stacks/cloudformation/components/win2k8_ami.rb
+++ stacks/cloudformation/components/win2k8_ami.rb
@@ -1,10 +1,10 @@
 SparkleFormation.build do
   mappings(:region_to_win2k8_ami) do
     # 	Windows_Server-2008-R2_SP1-English-64Bit-Base
-    set!('us-east-1'.disable_camel!, :ami => 'ami-3bd17a50')
-    set!('us-west-1'.disable_camel!, :ami => 'ami-ede01ea9')
-    set!('us-west-2'.disable_camel!, :ami => 'ami-59c3c969')
-    set!('eu-west-1'.disable_camel!, :ami => 'ami-82e6b4f5')
-    set!('eu-central-1'.disable_camel!, :ami => 'ami-0a181f17')
+    set!('us-east-1'.disable_camel!, :ami => 'ami-31620c54')
+    set!('us-west-1'.disable_camel!, :ami => 'ami-2b2dd76f')
+    set!('us-west-2'.disable_camel!, :ami => 'ami-d53c21e5')
+    set!('eu-west-1'.disable_camel!, :ami => 'ami-872f0ff0')
+    set!('eu-central-1'.disable_camel!, :ami => 'ami-98656585')
   end
 end
