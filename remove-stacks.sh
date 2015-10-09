99131f22ba5c59249a59ae2174258a7f443e7b2b
Greg Swallow <gswallow@gmail.com>
2015-10-09 13:45:04 -0400

    A bit of a refactor.

--- remove-stacks.sh
+++ remove-stacks.sh
@@ -4,24 +4,19 @@ region=${1-"us-east-1"}
 environment=${2-"dr"}
 
 get_stacks () {
-  aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE --region $region | \
-    jq '.StackSummaries[].StackName' | \
-    tr -d '"'
+  aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE UPDATE_COMPLETE --region $region \
+    --query 'StackSummaries[].StackName' --output text
 }
 
 get_stack_env () {
   stack=$1;
   shift;
 
-  local res=$(aws cloudformation describe-stacks --stack-name $stack --region $region | \
-    jq "if .Stacks[0].Tags[] | select(.Key == \"Environment\").Value == \"$environment\" then .Stacks[0].StackName else \"no\" end" | \
-    tr -d '"')
-  [[ $res == $stack ]] && return 0
-  return 1
+  aws cloudformation describe-stacks --stack-name $stack --region $region --query 'Stacks[0].Tags[?Key==`Environment`].Value' --output text
 }
 
 for i in $(get_stacks) ; do
-  if get_stack_env $i; then
+  if [ "$(get_stack_env $i)" == "$environment" ]; then
     aws cloudformation delete-stack --stack-name $i --region $region
   fi
 done
