c4f5951a3c73be7bbcec53f64d62f4fa958aa0ed
Greg Swallow <gswallow@gmail.com>
2015-10-14 10:52:10 -0400

    Add nexus role.

--- stacks/cloudformation/templates/sitemanager.rb
+++ stacks/cloudformation/templates/sitemanager.rb
@@ -1,6 +1,6 @@
 ENV['net_type'] ||= 'Private'
 ENV['sg']       ||= 'private_sg'
-ENV['run_list'] ||= 'role[base],role[site_manager]'
+ENV['run_list'] ||= 'role[base],role[site_manager],role[nexus]'
 
 require 'sparkle_formation'
 require_relative '../../../utils/environment'
