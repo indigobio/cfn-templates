66e623324f353112c14b8708b0599d1b9e96a5f0
Greg Swallow <gswallow@gmail.com>
2015-10-16 13:32:12 -0400

    Adjust order of replica set launches.

--- stacks/cloudformation/templates/databases_arbiter.rb
+++ stacks/cloudformation/templates/databases_arbiter.rb
@@ -1,9 +1,9 @@
-ENV['net_type']       ||= 'Private'
-ENV['sg']             ||= 'private_sg'
-ENV['volume_count']   ||= '8'
-ENV['volume_size']    ||= '250'
-ENV['run_list']       ||= 'role[base],role[tokumx_server]'
-ENV['third_run_list'] ||= 'role[base],role[tokumx_arbiter]' # Override with tokumx_arbiter if desired.
+ENV['net_type']         ||= 'Private'
+ENV['sg']               ||= 'private_sg'
+ENV['volume_count']     ||= '8'
+ENV['volume_size']      ||= '250'
+ENV['run_list']         ||= 'role[base],role[tokumx_server]'
+ENV['arbiter_run_list'] ||= 'role[base],role[tokumx_arbiter]'
 
 require 'sparkle_formation'
 require_relative '../../../utils/environment'
@@ -31,9 +31,10 @@ EOF
 
   dynamic!(:iam_instance_profile, 'database', :policy_statements => [ :create_snapshots ])
 
-  # Arbiter.
+  # Arbiter.  Comes up first because you can't set up a replica set on an arbiter,
+  # so you want a real mongo server up last.
   args = [
-    'thirddatabase',
+    'arbiter',
     :iam_instance_profile => :database_iam_instance_profile,
     :iam_instance_role => :database_iam_instance_role,
     :instance_type => 't2.small',
@@ -42,24 +43,22 @@ EOF
     :volume_size => 10,
     :ebs_optimized => false,
     :security_groups => lookup.get_security_groups(vpc),
-    :chef_run_list => ENV['third_run_list']
+    :chef_run_list => ENV['arbiter_run_list']
   ]
-  args.last.merge!(:snapshots => snapshots) unless snapshots.empty?
   dynamic!(:launch_config_chef_bootstrap, *args)
 
   dynamic!(:auto_scaling_group,
-           'thirddatabase',
-           :launch_config => :thirddatabase_launch_config,
+           'arbiter',
+           :launch_config => :arbiter_launch_config,
            :subnets => lookup.get_subnets(vpc),
            :notification_topic => lookup.get_notification_topic,
            :min_size => 0,
            :max_size => 1,
            :desired_capacity => 1)
 
-  # Two database cluster members; depend on the arbiter.  The idea is that the last server
-  # to run Chef will correctly set up a replicaset.
+  # First replica set member; depends on the arbiter.
   args = [
-      'database',
+      'firstdatabase',
       :iam_instance_profile => :database_iam_instance_profile,
       :iam_instance_role => :database_iam_instance_role,
       :instance_type => 't2.small',
@@ -73,13 +72,38 @@ EOF
   dynamic!(:launch_config_chef_bootstrap, *args)
 
   dynamic!(:auto_scaling_group,
-           'database',
+           'firstdatabase',
            :launch_config => :database_launch_config,
            :subnets => lookup.get_subnets(vpc),
            :notification_topic => lookup.get_notification_topic,
            :min_size => 1,
            :max_size => 2,
            :desired_capacity => 2,
-           :depends_on => 'ThirddatabaseAsg')
+           :depends_on => 'ArbiterAsg')
 
+  # Second replica set member; depends on the first member.  Running
+  # chef here will cause the replica set to initiate.
+  args = [
+      'seconddatabase',
+      :iam_instance_profile => :database_iam_instance_profile,
+      :iam_instance_role => :database_iam_instance_role,
+      :instance_type => 't2.small',
+      :create_ebs_volumes => true,
+      :volume_count => ENV['volume_count'].to_i,
+      :volume_size => ENV['volume_size'].to_i,
+      :security_groups => lookup.get_security_groups(vpc),
+      :chef_run_list => ENV['run_list']
+  ]
+  args.last.merge!(:snapshots => snapshots) unless snapshots.empty?
+  dynamic!(:launch_config_chef_bootstrap, *args)
+
+  dynamic!(:auto_scaling_group,
+           'seconddatabase',
+           :launch_config => :database_launch_config,
+           :subnets => lookup.get_subnets(vpc),
+           :notification_topic => lookup.get_notification_topic,
+           :min_size => 1,
+           :max_size => 2,
+           :desired_capacity => 2,
+           :depends_on => 'FirstdatabaseAsg')
 end
