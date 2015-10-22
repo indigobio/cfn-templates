a82f3e0c6b967ff844c4c4fb092d0482a2220afb
Greg Swallow <gswallow@gmail.com>
2015-10-22 18:33:45 -0400

    Change service logon.

--- stacks/cloudformation/components/win2k8_ami.rb
+++ stacks/cloudformation/components/win2k8_ami.rb
@@ -1,9 +1,9 @@
 SparkleFormation.build do
   mappings(:region_to_win2k8_ami) do
     # 	Windows_Server-2008-R2_SP1-English-64Bit-Base
-    set!('us-east-1'.disable_camel!, :ami => 'ami-13a0fc76')
-    set!('us-west-1'.disable_camel!, :ami => 'ami-f1f330b5')
-    set!('us-west-2'.disable_camel!, :ami => 'ami-149d7f27')
+    set!('us-east-1'.disable_camel!, :ami => 'ami-e3cf9386')
+    set!('us-west-1'.disable_camel!, :ami => 'ami-8df536c9')
+    set!('us-west-2'.disable_camel!, :ami => 'ami-feee0ccd')
     set!('eu-west-1'.disable_camel!, :ami => 'ami-872f0ff0')
     set!('eu-central-1'.disable_camel!, :ami => 'ami-98656585')
   end
