1bfa5302815199a482fdbf43e2ebaff635e14dbb
Greg Swallow <gswallow@gmail.com>
2015-10-22 11:56:16 -0400

    Reattempt our custom AMI.
    First one failed because chef credentials were left on disk.

--- stacks/cloudformation/components/win2k8_ami.rb
+++ stacks/cloudformation/components/win2k8_ami.rb
@@ -1,9 +1,9 @@
 SparkleFormation.build do
   mappings(:region_to_win2k8_ami) do
     # 	Windows_Server-2008-R2_SP1-English-64Bit-Base
-    set!('us-east-1'.disable_camel!, :ami => 'ami-2194c844')
-    set!('us-west-1'.disable_camel!, :ami => 'ami-4fea290b')
-    set!('us-west-2'.disable_camel!, :ami => 'ami-22896b11')
+    set!('us-east-1'.disable_camel!, :ami => 'ami-f786da92')
+    set!('us-west-1'.disable_camel!, :ami => 'ami-e3e92aa7')
+    set!('us-west-2'.disable_camel!, :ami => 'ami-428c6e71')
     set!('eu-west-1'.disable_camel!, :ami => 'ami-872f0ff0')
     set!('eu-central-1'.disable_camel!, :ami => 'ami-98656585')
   end
