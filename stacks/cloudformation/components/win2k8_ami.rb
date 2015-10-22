c05eda24cb2e75119f9e079060310df588f3cb00
Greg Swallow <gswallow@gmail.com>
2015-10-22 10:37:39 -0400

    Add our own AMI with LabSolutions & Xcalibur.

--- stacks/cloudformation/components/win2k8_ami.rb
+++ stacks/cloudformation/components/win2k8_ami.rb
@@ -1,9 +1,9 @@
 SparkleFormation.build do
   mappings(:region_to_win2k8_ami) do
     # 	Windows_Server-2008-R2_SP1-English-64Bit-Base
-    set!('us-east-1'.disable_camel!, :ami => 'ami-31620c54')
-    set!('us-west-1'.disable_camel!, :ami => 'ami-2b2dd76f')
-    set!('us-west-2'.disable_camel!, :ami => 'ami-d53c21e5')
+    set!('us-east-1'.disable_camel!, :ami => 'ami-2194c844')
+    set!('us-west-1'.disable_camel!, :ami => 'ami-4fea290b')
+    set!('us-west-2'.disable_camel!, :ami => 'ami-22896b11')
     set!('eu-west-1'.disable_camel!, :ami => 'ami-872f0ff0')
     set!('eu-central-1'.disable_camel!, :ami => 'ami-98656585')
   end
