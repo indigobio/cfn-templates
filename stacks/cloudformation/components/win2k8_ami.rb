dd403cc3b1e899e5e9d5c08121a05c81639a0841
Greg Swallow <gswallow@gmail.com>
2015-10-23 18:28:30 -0400

    Update windows AMI with mzconvert 1.2.9, shimadzu fixes.

--- stacks/cloudformation/components/win2k8_ami.rb
+++ stacks/cloudformation/components/win2k8_ami.rb
@@ -1,9 +1,9 @@
 SparkleFormation.build do
   mappings(:region_to_win2k8_ami) do
     # 	Windows_Server-2008-R2_SP1-English-64Bit-Base
-    set!('us-east-1'.disable_camel!, :ami => 'ami-e3cf9386')
-    set!('us-west-1'.disable_camel!, :ami => 'ami-8df536c9')
-    set!('us-west-2'.disable_camel!, :ami => 'ami-feee0ccd')
+    set!('us-east-1'.disable_camel!, :ami => 'ami-13eab576')
+    set!('us-west-1'.disable_camel!, :ami => 'ami-5f23e01b')
+    set!('us-west-2'.disable_camel!, :ami => 'ami-6410f257')
     set!('eu-west-1'.disable_camel!, :ami => 'ami-872f0ff0')
     set!('eu-central-1'.disable_camel!, :ami => 'ami-98656585')
   end
