41df92e32e000e06db44487a4b08e634717b88ab
Greg Swallow <gswallow@gmail.com>
2015-10-23 18:25:07 -0400

    Add ELB to custom reports servers.

--- stacks/cloudformation/templates/custom_reports.rb
+++ stacks/cloudformation/templates/custom_reports.rb
@@ -2,6 +2,8 @@ require 'sparkle_formation'
 require_relative '../../../utils/environment'
 require_relative '../../../utils/lookup'
 
+ENV['lb_purpose'] ||= 'customreports_elb'
+ENV['lb_name']    ||= "#{ENV['org']}-#{ENV['environment']}-cr-elb"
 ENV['net_type'] ||= 'Private'
 ENV['sg']       ||= 'web_sg'
 ENV['run_list'] ||= 'role[base],role[custom_reports]'
@@ -19,8 +21,28 @@ Run this template while running the compute, reporter and custom_reporter templa
 and databases templates.
 EOF
 
-  dynamic!(:iam_instance_profile, 'default')
+  parameters(:load_balancer_purpose) do
+    type 'String'
+    allowed_pattern "[\\x20-\\x7E]*"
+    default ENV['lb_purpose'] || 'none'
+    description 'Load Balancer Purpose tag to match, to associate custom reports instances.'
+    constraint_description 'can only contain ASCII characters'
+  end
 
-  dynamic!(:launch_config_chef_bootstrap, 'customreports', :instance_type => 't2.small', :create_ebs_volumes => false, :security_groups => lookup.get_security_groups(vpc), :chef_run_list => ENV['run_list'])
+  dynamic!(:elb, 'customreports',
+           :listeners => [
+               { :instance_port => '80', :instance_protocol => 'tcp', :load_balancer_port => '80', :protocol => 'tcp' }
+           ],
+           :security_groups => lookup.get_security_groups(vpc),
+           :subnets => lookup.get_subnets(vpc),
+           :lb_name => ENV['lb_name'],
+           :scheme => 'internal'
+  )
+
+  dynamic!(:iam_instance_profile, 'default', :policy_statements => [ :modify_elbs ])
+
+  dynamic!(:launch_config_chef_bootstrap, 'customreports', :instance_type => 't2.small', :create_ebs_volumes => false, :security_groups => lookup.get_security_groups(vpc), :chef_run_list => ENV['run_list'], :extra_bootstrap => 'register_with_elb')
   dynamic!(:auto_scaling_group, 'customreports', :launch_config => :customreports_launch_config, :subnets => lookup.get_subnets(vpc), :notification_topic => lookup.get_notification_topic)
+
+  dynamic!(:route53_record_set, 'customreports_elb', :record => 'customreports', :target => :customreports_elb, :domain_name => ENV['private_domain'], :attr => 'DNSName', :ttl => '60')
 end
