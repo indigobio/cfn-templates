80108743a9495f5490472265c12b2a3fa067759f
Greg Swallow <gswallow@gmail.com>
2015-10-24 12:49:01 -0400

    New AMI.  GCC 4.9 made default.

--- stacks/cloudformation/components/precise_ruby22_ami.rb
+++ stacks/cloudformation/components/precise_ruby22_ami.rb
@@ -1,9 +1,9 @@
 SparkleFormation.build do
   # For simplicity's sake we'll just use hvm:ebs-ssd AMIs for now. Currently based on 12.04.5 Release 20150728
   mappings(:region_to_precise_ami) do
-    set!('us-east-1'.disable_camel!, :ami => 'ami-613c7e04') # ami-a7558fcc
-    set!('us-west-1'.disable_camel!, :ami => 'ami-0130f645') # ami-039b6747
-    set!('us-west-2'.disable_camel!, :ami => 'ami-42a84c71') # ami-2d6e601d
+    set!('us-east-1'.disable_camel!, :ami => 'ami-b5d58bd0') # ami-a7558fcc
+    set!('us-west-1'.disable_camel!, :ami => 'ami-37503c57') # ami-039b6747
+    set!('us-west-2'.disable_camel!, :ami => 'ami-7a51b349') # ami-2d6e601d
     set!('eu-west-1'.disable_camel!, :ami => 'ami-42722635')
     set!('eu-central-1'.disable_camel!, :ami => 'ami-38b8bd25')
   end
