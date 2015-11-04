e9fc2c10956722fd81a9ad0af8a67d88dfea8071
Greg Swallow <gswallow@gmail.com>
2015-11-04 16:45:48 -0500

    Ok so maybe using an m1.small was a bad idea.
    Launching a new EC2 instance. Status Reason: Your requested instance type (m1.small) is not supported in your requested Availability Zone (us-east-1e).

--- vpc/cloudformation/components/nat_ami.rb
+++ vpc/cloudformation/components/nat_ami.rb
@@ -1,9 +1,7 @@
-# READ ME: changed from HVM to PV because m1.smalls may be faster than m3.mediums
-# You may very well want to switch to an HVM AMI if you intend to use newer generation instance types.
 SparkleFormation.build do
   mappings.region_to_nat_ami do
-    set!('us-east-1'.disable_camel!,    :ami => 'ami-5fb8c835') # amzn-ami-hvm-2014.09.2.x86_64-ebs (NOT the nat ami)
-    set!('us-west-1'.disable_camel!,    :ami => 'ami-56ea8636')
-    set!('us-west-2'.disable_camel!,    :ami => 'ami-d93622b8')
+    set!('us-east-1'.disable_camel!,    :ami => 'ami-1ecae776') # amzn-ami-hvm-2014.09.2.x86_64-ebs (NOT the nat ami)
+    set!('us-west-1'.disable_camel!,    :ami => 'ami-d114f295')
+    set!('us-west-2'.disable_camel!,    :ami => 'ami-e7527ed7')
   end
 end
