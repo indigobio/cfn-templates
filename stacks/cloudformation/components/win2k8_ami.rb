7faf118d7f905e55bc6d7890df34f15bb1ce5397
Greg Swallow <gswallow@gmail.com>
2016-02-25 16:11:15 -0500

    Grr windows AMIs.

--- stacks/cloudformation/components/win2k8_ami.rb
+++ stacks/cloudformation/components/win2k8_ami.rb
@@ -1,7 +1,7 @@
 SparkleFormation.build do
   mappings(:region_to_win2k8_ami) do
     # 	Windows_Server-2008-R2_SP1-English-64Bit-Base
-    set!('us-east-1'.disable_camel!, :ami => 'ami-315f625b')
+    set!('us-east-1'.disable_camel!, :ami => 'ami-51c0fd3b')
     set!('us-west-1'.disable_camel!, :ami => 'ami-29077749')
     set!('us-west-2'.disable_camel!, :ami => 'ami-2c57b54c')
   end
