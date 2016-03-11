02fe17b5654b28d66d3003e924da6fe0a6e468dd
Greg Swallow <gswallow@gmail.com>
2016-03-10 19:53:42 -0500

    Fix whatsinstalled template.

--- stacks/cloudformation/templates/whatsinstalled.rb
+++ stacks/cloudformation/templates/whatsinstalled.rb
@@ -2,19 +2,17 @@ require 'sparkle_formation'
 require_relative '../../../utils/environment'
 require_relative '../../../utils/lookup'
 
-ENV['lb_purpose'] ||= 'nexus_elb'
-ENV['lb_name']    ||= "#{ENV['org']}-#{ENV['environment']}-nexus-elb"
 ENV['net_type'] ||= 'Private'
 ENV['sg']       ||= 'web_sg'
-ENV['run_list'] ||= 'role[base],role[site_manager],role[nexus]'
+ENV['run_list'] ||= 'role[base],role[whatsinstalled]'
 
 lookup = Indigo::CFN::Lookups.new
 vpc = lookup.get_vpc
 
-SparkleFormation.new('nexus').load(:precise_ruby22_ami, :ssh_key_pair, :chef_validator_key_bucket).overrides do
+SparkleFormation.new('whatsinstalled').load(:precise_ruby22_ami, :ssh_key_pair, :chef_validator_key_bucket).overrides do
   set!('AWSTemplateFormatVersion', '2010-09-09')
   description <<EOF
-Creates an auto scaling groups containing site (customer) management instances.
+Creates an auto scaling groups containing whatsinstalled instances.
 Each instance is given an IAM instance profile, which allows the instance to get validator keys and encrypted
 data bag secrets from the Chef validator key bucket.
 
@@ -22,29 +20,7 @@ Launch this template after launching the fileserver and assaymatic templates.  L
 a VPC with a matching environment, assaymatic servers, and a file server.
 EOF
 
-  parameters(:load_balancer_purpose) do
-    type 'String'
-    allowed_pattern "[\\x20-\\x7E]*"
-    default ENV['lb_purpose'] || 'none'
-    description 'Load Balancer Purpose tag to match, to associate nexus instances.'
-    constraint_description 'can only contain ASCII characters'
-  end
-
-  dynamic!(:elb, 'nexus',
-           :listeners => [
-               { :instance_port => '80', :instance_protocol => 'http', :load_balancer_port => '80', :protocol => 'http' },
-               { :instance_port => '8080', :instance_protocol => 'http', :load_balancer_port => '8080', :protocol => 'http' }
-           ],
-           :security_groups => lookup.get_security_group_ids(vpc),
-           :subnets => lookup.get_subnets(vpc),
-           :lb_name => ENV['lb_name'],
-           :idle_timeout => '300',
-           :scheme => 'internal'
-  )
-
-  dynamic!(:iam_instance_profile, 'default', :policy_statements => [ :chef_bucket_access, :modify_elbs ])
-  dynamic!(:launch_config_chef_bootstrap, 'nexus', :instance_type => 't2.small', :security_groups => lookup.get_security_group_ids(vpc), :chef_run_list => ENV['run_list'])
-  dynamic!(:auto_scaling_group, 'nexus', :launch_config => :nexus_launch_config, :subnets => lookup.get_subnets(vpc), :load_balancers => [ ref!('NexusElb') ], :notification_topic => lookup.get_notification_topic)
-
-  dynamic!(:route53_record_set, 'nexus_elb', :record => 'nexus', :target => :nexus_elb, :domain_name => ENV['private_domain'], :attr => 'DNSName', :ttl => '60')
+  dynamic!(:iam_instance_profile, 'default', :policy_statements => [ :chef_bucket_access, :modify_route53 ])
+  dynamic!(:launch_config_chef_bootstrap, 'whatsinstalled', :instance_type => 't2.small', :security_groups => lookup.get_security_group_ids(vpc), :chef_run_list => ENV['run_list'])
+  dynamic!(:auto_scaling_group, 'whatsinstalled', :launch_config => :whatsinstalled_launch_config, :max_size => 1, :desired_capacity => 1, :subnets => lookup.get_subnets(vpc), :notification_topic => lookup.get_notification_topic)
 end
