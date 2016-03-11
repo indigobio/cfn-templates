aa6a0c27eb9750069b9f31fff8594c8081f3d967
Greg Swallow <gswallow@gmail.com>
2016-03-11 16:55:03 -0500

    Cloudfront tweaks.

--- stacks/cloudformation/templates/cloudfront.rb
+++ stacks/cloudformation/templates/cloudfront.rb
@@ -2,14 +2,10 @@ require 'sparkle_formation'
 require_relative '../../../utils/environment'
 require_relative '../../../utils/lookup'
 
-SparkleFormation.new('webserver').load(:precise_ruby22_ami, :ssh_key_pair, :chef_validator_key_bucket).overrides do
+SparkleFormation.new('webserver').overrides do
   set!('AWSTemplateFormatVersion', '2010-09-09')
   description <<EOF
-Creates an auto scaling group containing webserver instances.  Each instance is given an IAM instance
-profile, which allows the instance to get objects from the Chef Validator Key Bucket.
-
-Run this template while running the compute, reporter and custom_reporter templates.  Depends on the rabbitmq
-and databases templates.
+Creates a Cloudfront distribution and an S3 bucket to hold public assets.
 EOF
 
   dynamic!(:s3_bucket, 'assets', :acl => 'PublicRead')
--- utils/jenkins/workflow-recovery.groovy
+++ utils/jenkins/workflow-recovery.groovy
@@ -32,6 +32,18 @@ parallel first: {
         ]
 }, third: {
   try {
+  build job: '130-launch-cloudfront',
+  parameters: [
+    [$class: 'TextParameterValue', name: 'environment', value: workflow_env],
+    [$class: 'TextParameterValue', name: 'region', value: workflow_aws_region],
+    [$class: 'CredentialsParameterValue', description: '', name: 'workflow_aws_access_key_id', value: workflow_aws_access_key_id],
+    [$class: 'CredentialsParameterValue', description: '', name: 'workflow_aws_secret_access_key', value: workflow_aws_secret_access_key]
+  ]
+  } catch (Exception e) {
+  echo 'Whoops.  Launching the vpn failed. ' + e // TODO: send notifications
+  }
+}, fourth: {
+  try {
     build job: '210-launch-vpn',
           parameters: [
             [$class: 'TextParameterValue', name: 'environment', value: workflow_env],
@@ -43,7 +55,7 @@ parallel first: {
   } catch (Exception e) {
     echo 'Whoops.  Launching the vpn failed. ' + e // TODO: send notifications
   }
-}, fourth: {
+}, fifth: {
   try {
     build job: '220-launch-whatsinstalled',
           parameters: [
