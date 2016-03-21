8dc595f5e9b97a23d97a2678b822a1741173fbfc
Greg Swallow <gswallow@gmail.com>
2016-03-21 19:27:35 -0400

    Update mzconvert ami; not the empire ami (though that's coming).

--- empire/cloudformation/components/empire_ami.rb
+++ empire/cloudformation/components/empire_ami.rb
@@ -1,7 +1,7 @@
 SparkleFormation.build do
   # For simplicity's sake we'll just use hvm:ebs-ssd AMIs for now. Currently based on 14.04.3 Release 20150728
   mappings(:region_to_empire_ami) do
-    set!('us-east-1'.disable_camel!, :ami => 'ami-53a3a339')
+    set!('us-east-1'.disable_camel!, :ami => 'ami-0bdfd261')
     set!('us-west-1'.disable_camel!, :ami => 'ami-c57f0da5')
     set!('us-west-2'.disable_camel!, :ami => 'ami-e901e889')
   end
--- stacks/cloudformation/components/win2k8_ami.rb
+++ stacks/cloudformation/components/win2k8_ami.rb
@@ -1,7 +1,7 @@
 SparkleFormation.build do
   mappings(:region_to_win2k8_ami) do
     # 	Windows_Server-2008-R2_SP1-English-64Bit-Base
-    set!('us-east-1'.disable_camel!, :ami => 'ami-51c0fd3b')
+    set!('us-east-1'.disable_camel!, :ami => 'ami-0bdfd261')
     set!('us-west-1'.disable_camel!, :ami => 'ami-dc5727bc')
     set!('us-west-2'.disable_camel!, :ami => 'ami-48f11c28')
   end
