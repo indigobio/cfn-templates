1df3b6816b2b7df4d5d6f2ed2aa3b27751abe69b
Greg Swallow <gswallow@gmail.com>
2016-03-21 20:28:51 -0400

    WIP use swap.

--- empire/cloudformation/components/empire_ami.rb
+++ empire/cloudformation/components/empire_ami.rb
@@ -1,8 +1,8 @@
 SparkleFormation.build do
   # For simplicity's sake we'll just use hvm:ebs-ssd AMIs for now. Currently based on 14.04.3 Release 20150728
   mappings(:region_to_empire_ami) do
-    set!('us-east-1'.disable_camel!, :ami => 'ami-0bdfd261')
-    set!('us-west-1'.disable_camel!, :ami => 'ami-c57f0da5')
-    set!('us-west-2'.disable_camel!, :ami => 'ami-e901e889')
+    set!('us-east-1'.disable_camel!, :ami => 'ami-7fe7ea15')
+    set!('us-west-1'.disable_camel!, :ami => 'ami-d881f3b8')
+    set!('us-west-2'.disable_camel!, :ami => 'ami-ec25cd8c')
   end
 end
