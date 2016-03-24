d235ce5ef51316518ca5388592c254a2e54bbaf9
Greg Swallow <gswallow@gmail.com>
2016-03-24 13:56:40 -0400

    Bump ami.

--- empire/cloudformation/components/empire_ami.rb
+++ empire/cloudformation/components/empire_ami.rb
@@ -1,8 +1,8 @@
 SparkleFormation.build do
   # For simplicity's sake we'll just use hvm:ebs-ssd AMIs for now. Currently based on 14.04.3 Release 20150728
   mappings(:region_to_empire_ami) do
-    set!('us-east-1'.disable_camel!, :ami => 'ami-f9dad593')
-    set!('us-west-1'.disable_camel!, :ami => 'ami-6d3d400d')
-    set!('us-west-2'.disable_camel!, :ami => 'ami-4fe9022f')
+    set!('us-east-1'.disable_camel!, :ami => 'ami-49eee123')
+    set!('us-west-1'.disable_camel!, :ami => 'ami-e2324f82')
+    set!('us-west-2'.disable_camel!, :ami => 'ami-89ea01e9')
   end
 end
