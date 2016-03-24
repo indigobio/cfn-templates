4f02e27528dbb16771890d0cc2bee09804e550a4
Greg Swallow <gswallow@gmail.com>
2016-03-24 10:14:03 -0400

    Clean up detritus left by empire.

---
+++ utils/zap-ecs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+environment=$1
+
+for cluster in $(aws ecs list-clusters --query 'clusterArns[]' --output table | grep indigo-$environment-empire | awk '{print $2}'); do
+  for service in $(aws ecs list-services --cluster $cluster --query 'serviceArns[]' --output text); do
+    aws ecs update-service --cluster $cluster --service $service --desired-count 0
+    aws ecs delete-service --cluster $cluster --service $service
+  done
+  aws ecs delete-cluster --cluster $cluster
+done
+
