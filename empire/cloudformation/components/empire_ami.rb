2f8433cf9c9d2b5b98330d9b7197ff62459f5242
Greg Swallow <gswallow@gmail.com>
2016-03-24 15:05:49 -0400

    Update empire ami :/

--- empire/cloudformation/components/empire_ami.rb
+++ empire/cloudformation/components/empire_ami.rb
@@ -1,8 +1,8 @@
 SparkleFormation.build do
   # For simplicity's sake we'll just use hvm:ebs-ssd AMIs for now. Currently based on 14.04.3 Release 20150728
   mappings(:region_to_empire_ami) do
-    set!('us-east-1'.disable_camel!, :ami => 'ami-49eee123')
-    set!('us-west-1'.disable_camel!, :ami => 'ami-e2324f82')
-    set!('us-west-2'.disable_camel!, :ami => 'ami-89ea01e9')
+    set!('us-east-1'.disable_camel!, :ami => 'ami-44e4eb2e')
+    set!('us-west-1'.disable_camel!, :ami => 'ami-d3334eb3')
+    set!('us-west-2'.disable_camel!, :ami => 'ami-41e30821')
   end
 end
