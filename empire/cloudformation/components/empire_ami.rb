903d9cf3deed98201df6930ef00606dad644db47
Greg Swallow <gswallow@gmail.com>
2016-03-31 14:49:30 -0400

    Update ami for ecs agent 1.8.1 and changes to sumologic.

--- empire/cloudformation/components/empire_ami.rb
+++ empire/cloudformation/components/empire_ami.rb
@@ -1,8 +1,8 @@
 SparkleFormation.build do
   # For simplicity's sake we'll just use hvm:ebs-ssd AMIs for now. Currently based on 14.04.3 Release 20150728
   mappings(:region_to_empire_ami) do
-    set!('us-east-1'.disable_camel!, :ami => 'ami-44e4eb2e')
-    set!('us-west-1'.disable_camel!, :ami => 'ami-d3334eb3')
-    set!('us-west-2'.disable_camel!, :ami => 'ami-41e30821')
+    set!('us-east-1'.disable_camel!, :ami => 'ami-7c454f16')
+    set!('us-west-1'.disable_camel!, :ami => 'ami-cefb86ae')
+    set!('us-west-2'.disable_camel!, :ami => 'ami-946d87f4')
   end
 end
