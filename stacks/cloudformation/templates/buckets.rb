ca91846b57be8aadd12b401c7a84a54399dfb876
Greg Swallow <gswallow@gmail.com>
2016-05-11 16:35:00 -0400

    Fix bucket names.

--- stacks/cloudformation/templates/buckets.rb
+++ stacks/cloudformation/templates/buckets.rb
@@ -8,15 +8,15 @@ SparkleFormation.new('buckets').overrides do
 Creates S3 buckets to hold data in transit.
 EOF
 
-  dynamic!(:s3_bucket, 'archival', :bucket_name => "asc-#{ENV['environment']}-archival", :acl => 'BucketOwnerFullControl')
+  dynamic!(:s3_bucket, 'archival', :bucket_name => "ascent-#{ENV['environment']}-archival", :acl => 'BucketOwnerFullControl')
   dynamic!(:s3_owner_write_bucket_policy, 'archival', :bucket => 'AssetsS3Bucket')
 
-  dynamic!(:s3_bucket, 'extract', :bucket_name => "asc-#{ENV['environment']}-extract", :acl => 'BucketOwnerFullControl')
+  dynamic!(:s3_bucket, 'extract', :bucket_name => "ascent-#{ENV['environment']}-extract", :acl => 'BucketOwnerFullControl')
   dynamic!(:s3_owner_write_bucket_policy, 'archival', :bucket => 'ExtractS3Bucket')
 
-  dynamic!(:s3_bucket, 'raw', :bucket_name => "asc-#{ENV['environment']}-raw", :acl => 'BucketOwnerFullControl')
+  dynamic!(:s3_bucket, 'raw', :bucket_name => "ascent-#{ENV['environment']}-raw", :acl => 'BucketOwnerFullControl')
   dynamic!(:s3_owner_write_bucket_policy, 'raw', :bucket => 'RawS3Bucket')
 
-  dynamic!(:s3_bucket, 'customreports', :bucket_name => "asc-#{ENV['environment']}-customreports", :acl => 'BucketOwnerFullControl')
+  dynamic!(:s3_bucket, 'customreports', :bucket_name => "ascent-#{ENV['environment']}-customreports", :acl => 'BucketOwnerFullControl')
   dynamic!(:s3_owner_write_bucket_policy, 'customreports', :bucket => 'CustomreportsS3Bucket')
 end
