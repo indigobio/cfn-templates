d0281aadd4855cc9b8e3508a2df7af5a219829ae
Greg Swallow <gswallow@gmail.com>
2016-05-23 10:48:08 -0400

    Delete ECS container instances (brute force).
    The tool may need to be run twice as there appear to be race conditions deleting tasks, etc.

--- utils/zap-ecs.sh
+++ utils/zap-ecs.sh
@@ -7,6 +7,20 @@ for cluster in $(aws ecs list-clusters --query 'clusterArns[]' --output table | 
     aws ecs update-service --cluster $cluster --service $service --desired-count 0
     aws ecs delete-service --cluster $cluster --service $service
   done
+  for instance in $(aws ecs list-container-instances --cluster $cluster --query 'containerInstanceArns[]' --output text); do
+    id=$(aws ecs describe-container-instances --cluster $cluster --container-instances $instance --query 'containerInstances[].ec2InstanceId' --output text)
+    aws ecs deregister-container-instance --cluster $cluster --container-instance $instance
+    answer='n'
+    echo -n "Terminate instance $id? [Y/n] "
+    read answer
+    case $answer in
+      Y|y)
+        aws ec2 terminate-instances --instance-ids $id
+      ;;
+      *)
+      ;;
+    esac
+  done
   aws ecs delete-cluster --cluster $cluster
 done
 
