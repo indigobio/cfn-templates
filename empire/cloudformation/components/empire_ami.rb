c03bbeac062e450f02493e3dbab40aba61337b41
Greg Swallow <gswallow@gmail.com>
2016-06-21 10:14:34 -0400

    [AR-1896] Update base ami for docker 1.11.2

--- empire/cloudformation/components/empire_ami.rb
+++ empire/cloudformation/components/empire_ami.rb
@@ -1,8 +1,8 @@
 SparkleFormation.build do
   # For simplicity's sake we'll just use hvm:ebs-ssd AMIs for now. Currently based on 14.04.3 Release 20150728
   mappings(:region_to_empire_ami) do
-    set!('us-east-1'.disable_camel!, :ami => 'ami-1568757f')
-    set!('us-west-1'.disable_camel!, :ami => 'ami-8a037cea')
-    set!('us-west-2'.disable_camel!, :ami => 'ami-18699e78')
+    set!('us-east-1'.disable_camel!, :ami => 'ami-924c8cff')
+    set!('us-west-1'.disable_camel!, :ami => 'ami-751e5a15')
+    set!('us-west-2'.disable_camel!, :ami => 'ami-55fd3935')
   end
 end
