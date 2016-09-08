f136f4a7d07bf5032c204696022c6152a37124bb
Greg Swallow <gswallow@gmail.com>
2016-09-08 12:51:55 -0400

    Fix IAM role to allow mongo-ec2-backup to share snapshots.

--- stacks/cloudformation/registry/create_snapshots.rb
+++ stacks/cloudformation/registry/create_snapshots.rb
@@ -3,6 +3,7 @@ SfnRegistry.register(:create_snapshots) do
   { 'Action' => %w(ec2:CreateSnapshot
                    ec2:DeleteSnapshot
                    ec2:DescribeSnapshots
+                   ec2:ModifySnapshotAttribute
                    ec2:AttachVolume
                    ec2:CreateVolume
                    ec2:ModifyVolumeAttribute
