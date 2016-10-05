8f8d0d905a5ae289783aaf60fea56e2773215c79
Greg Swallow <gswallow@gmail.com>
2016-10-05 08:57:33 -0400

    Update to 9.4.9

--- rds/cloudformation/components/engine_versions.rb
+++ rds/cloudformation/components/engine_versions.rb
@@ -4,7 +4,7 @@ SparkleFormation.build do
     camel_keys_set!(:auto_disable)
     set!('MySQL', :version => '5.6.23')
     set!('oracle-ee', :version => '12.2.0.1.v1')
-    set!('postgres', :version => '9.4.7')
+    set!('postgres', :version => '9.4.9')
     set!('sqlserver-se', :version => '11.00.2100.60.v1')
     set!('sqlserver-ex', :version => '11.00.2100.60.v1')
     set!('sqlserver-web', :version => '11.00.2100.60.v1')
