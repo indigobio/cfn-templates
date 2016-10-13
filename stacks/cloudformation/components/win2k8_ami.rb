52f19a2460fa031266d0a154c20e16be16f3466f
Greg Swallow <gswallow@gmail.com>
2016-10-13 10:43:24 -0400

    Update windows AMI.
    Shimadzu demo license expires soon.

--- stacks/cloudformation/components/win2k8_ami.rb
+++ stacks/cloudformation/components/win2k8_ami.rb
@@ -1,8 +1,8 @@
 SparkleFormation.build do
   mappings(:region_to_win2k8_ami) do
     # 	Windows_Server-2008-R2_SP1-English-64Bit-Base
-    set!('us-east-1'.disable_camel!, :ami => 'ami-0bdfd261')
-    set!('us-west-1'.disable_camel!, :ami => 'ami-e7532f87')
-    set!('us-west-2'.disable_camel!, :ami => 'ami-ecba4e8c')
+    set!('us-east-1'.disable_camel!, :ami => 'ami-304d0727')
+    set!('us-west-1'.disable_camel!, :ami => 'ami-f1397191')
+    set!('us-west-2'.disable_camel!, :ami => 'ami-ef78a38f')
   end
 end
