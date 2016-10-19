f2927303777701ac1996244812491bfccf074cd5
Greg Swallow <gswallow@gmail.com>
2016-10-19 19:04:06 -0400

    Create a script that builds psycopg2 libraries for Amazon linux.

--- lambda/functions/rds_create_role/create_deployment_package.sh
+++ lambda/functions/rds_create_role/create_deployment_package.sh
@@ -1,49 +1,91 @@
 #!/usr/bin/env bash
 
-# Run this on an Amazon Linux EC2 instance *or in a container*
+# aws ec2 describe-images --owners 137112412989 \
+#  --filters 'Name=block-device-mapping.volume-type,Values=gp2' \
+#  'Name=virtualization-type,Values=hvm' \
+#  --query 'Images[*].{Name:Name,ID:ImageId}' \
+#  --output text \
+#  | sort -k 2 \
+#  | grep -v vpc-nat
 
-sudo yum -y update
-sudo yum -y groupinstall "Development Tools"
-sudo yum -y install openssl-devel
-sudo yum -y install Cython --enablerepo=epel
+stamp="temp-$(date +'%Y%m%d%H%M%S')$$"
 
-wget https://ftp.postgresql.org/pub/source/v9.4.9/postgresql-9.4.9.tar.gz
-wget http://initd.org/psycopg/tarballs/PSYCOPG-2-6/psycopg2-2.6.2.tar.gz
-tar xzf postgresql-9.4.9.tar.gz
-tar xzf psycopg2-2.6.2.tar.gz
+echo "Creating temporary key pair for ssh."
+aws ec2 create-key-pair --key-name $stamp --query 'KeyMaterial' --output text > ${stamp}-rsa 
+chmod go-rwx ${stamp}-rsa
 
-cd postgresql-9.4.9
-./configure --prefix=`pwd` --without-readline --without-zlib --with-openssl && make && make install
-cd ..
+myvpc=$(aws ec2 describe-vpcs --filters 'Name=isDefault,Values=true' --query 'Vpcs[0].VpcId')
+myvpc=$(eval echo $myvpc)
 
-mkdir /tmp/psycopg2
-cd psycopg2-2.6.2
-cat > setup.cfg <<EOF
-[build_ext]
-define =
-use_pydatetime = 1
-have_ssl = 1
-pg_config = ../postgresql-9.4.9/bin/pg_config
-static_libpq = 1
-libraries=ssl crypto
+mysg=$(aws ec2 create-security-group --group-name $stamp --vpc-id $myvpc --description 'SSH access for deployment package build' --query 'GroupId')
+mysg=$(eval echo $mysg)
+echo -n "Waiting for security group $mysg to be created in $myvpc."
+while ! aws ec2 describe-security-groups --group-ids $mysg &> /dev/null; do
+  echo -n "."
+  sleep 5
+done
+echo
 
-[egg_info]
-tag_build =
-tag_date = 0
-tag_svn_revision = 0
-EOF
-python setup.py build
-python setup.py install --root /tmp/psycopg2
-cd ..
+aws ec2 authorize-security-group-ingress --group-id $mysg --protocol tcp --port 22 --cidr 0.0.0.0/0
 
-virtualenv /tmp/rds_create_role
+amzn_ami=$(aws ec2 describe-images --owners 137112412989 \
+ --filters 'Name=block-device-mapping.volume-type,Values=gp2' \
+ 'Name=virtualization-type,Values=hvm' \
+ --query 'Images[*].{Name:Name,ID:ImageId}' \
+ --output text \
+ | sort -k 2 \
+ | grep -v vpc-nat \
+ | grep -v 'rc-' \
+ | tail -1 \
+ | awk '{print $1}')
 
-pushd /tmp/psycopg2/usr/lib64/python2.7/dist-packages
-tar cf - . | ( cd /tmp/rds_create_role ; tar xvpBf - )
-popd
+amzn_ami=$(eval echo $amzn_ami)
 
-pushd /tmp/rds_create_role
-zip -r /tmp/rds_create_role.zip .
-popd
+echo "Launching an instance from $amzn_ami"
+myinst=$(aws ec2 run-instances \
+         --image-id $amzn_ami \
+         --key-name $stamp \
+         --security-group-ids $mysg \
+         --instance-type c4.large \
+         --count 1 \
+         --associate-public-ip-address \
+         --query 'Instances[0].InstanceId')
 
-cp /tmp/rds_create_role.zip .
+myinst=$(eval echo $myinst)
+
+echo -n "Waiting on a public IP address."
+myip=$(aws ec2 describe-instances --instance-id $myinst --query 'Reservations[0].Instances[0].PublicIpAddress')
+while test $myip == 'null'; do
+  echo -n "."
+  myip=$(aws ec2 describe-instances --instance-id $myinst --query 'Reservations[0].Instances[0].PublicIpAddress')
+  sleep 1
+done
+echo
+myip=$(eval echo $myip)
+
+echo -n "Waiting on ssh to $myip."
+while ! nc -z -w 1 $myip 22; do
+  echo -n "."
+  sleep 1
+done
+echo
+
+echo "Logging in."
+echo 'yes' | scp -o StrictHostKeyChecking=no -q -i ${stamp}-rsa remote.sh ec2-user@${myip}: && \
+ssh -t -i ${stamp}-rsa ec2-user@${myip} chmod 755 remote.sh && \
+ssh -t -i ${stamp}-rsa ec2-user@${myip} ./remote.sh && \
+scp -i ${stamp}-rsa ec2-user@${myip}:rds_create_role.zip . && \
+zip -u rds_create_role.zip rds_create_role.py
+
+#aws ec2 terminate-instances --instance-id $myinst &> /dev/null
+#
+#echo -n "Waiting on $myinst to be terminated."
+#while test $(aws ec2 describe-instances --instance-id $myinst --query 'Reservations[0].Instances[0].State.Name' --output text) != 'terminated'; do
+#  echo -n "."
+#  sleep 10
+#done
+#echo
+#
+#aws ec2 delete-security-group --group-id $mysg
+#aws ec2 delete-key-pair --key-name $stamp
+#rm ${stamp}-rsa
