9b1d275e7d02f8e9a42ddb87299ec9feb1514cd5
Greg Swallow <gswallow@gmail.com>
2016-11-18 14:57:00 -0500

    Update AMI and ECS agent.

--- empire/cloudformation/components/empire_ami.rb
+++ empire/cloudformation/components/empire_ami.rb
@@ -1,8 +1,9 @@
 SparkleFormation.build do
   # For simplicity's sake we'll just use hvm:ebs-ssd AMIs for now. Currently based on 14.04.3 Release 20150728
   mappings(:region_to_empire_ami) do
-    set!('us-east-1'.disable_camel!, :ami => 'ami-971d8480')
-    set!('us-west-1'.disable_camel!, :ami => 'ami-5abaf93a')
-    set!('us-west-2'.disable_camel!, :ami => 'ami-0860ab68')
+    set!('us-east-1'.disable_camel!, :ami => 'ami-f3be8be4')
+    set!('us-east-2'.disable_camel!, :ami => 'ami-95603af0')
+    set!('us-west-1'.disable_camel!, :ami => 'ami-99f8adf9')
+    set!('us-west-2'.disable_camel!, :ami => 'ami-8012bfe0')
   end
 end
