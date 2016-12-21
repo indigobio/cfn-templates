9e2cc4a76b3c7247f7f9d85259b7e2a1968dc4b4
Greg Swallow <gswallow@gmail.com>
2016-12-21 14:57:31 -0500

    Ubuntu 16.04

--- empire/cloudformation/components/empire_ami.rb
+++ empire/cloudformation/components/empire_ami.rb
@@ -1,9 +1,9 @@
 SparkleFormation.build do
   # For simplicity's sake we'll just use hvm:ebs-ssd AMIs for now. Currently based on 14.04.3 Release 20150728
   mappings(:region_to_empire_ami) do
-    set!('us-east-1'.disable_camel!, :ami => 'ami-f3be8be4')
-    set!('us-east-2'.disable_camel!, :ami => 'ami-95603af0')
-    set!('us-west-1'.disable_camel!, :ami => 'ami-99f8adf9')
-    set!('us-west-2'.disable_camel!, :ami => 'ami-8012bfe0')
+    set!('us-east-1'.disable_camel!, :ami => 'ami-04cade13')
+    set!('us-east-2'.disable_camel!, :ami => 'ami-90c79df5')
+    set!('us-west-1'.disable_camel!, :ami => 'ami-61550401')
+    set!('us-west-2'.disable_camel!, :ami => 'ami-f7d06797')
   end
 end
