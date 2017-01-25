722e44b6d4f9445a504269b8a6358bb980d30595
Greg Swallow <gswallow@gmail.com>
2017-01-25 14:57:34 -0500

    us-east-2 support.

--- vpc/cloudformation/components/nat_ami.rb
+++ vpc/cloudformation/components/nat_ami.rb
@@ -1,7 +1,8 @@
 SparkleFormation.build do
   mappings.region_to_nat_ami do
-    set!('us-east-1'.disable_camel!,    :ami => 'ami-f5f41398') # amzn-ami-hvm-2015.09.2.x86_64-ebs (NOT the nat ami)
-    set!('us-west-1'.disable_camel!,    :ami => 'ami-6e84fa0e')
-    set!('us-west-2'.disable_camel!,    :ami => 'ami-d0f506b0')
+    set!('us-east-1'.disable_camel!,    :ami => 'ami-0b33d91d') # amzn-ami-hvm-2015.09.2.x86_64-ebs (NOT the nat ami)
+    set!('us-east-2'.disable_camel!,    :ami => 'ami-c55673a0')
+    set!('us-west-1'.disable_camel!,    :ami => 'ami-165a0876')
+    set!('us-west-2'.disable_camel!,    :ami => 'ami-f173cc91')
   end
 end
