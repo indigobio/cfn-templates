842a54ed591ba3de19c794645c395690ae506360
Greg Swallow <gswallow@gmail.com>
2017-01-27 19:17:10 -0500

    Roll back AMI to avoid systemd/ansible bug.
    https://github.com/ansible/ansible/issues/20332

--- empire/cloudformation/components/empire_ami.rb
+++ empire/cloudformation/components/empire_ami.rb
@@ -1,9 +1,9 @@
 SparkleFormation.build do
   # For simplicity's sake we'll just use hvm:ebs-ssd AMIs for now. Currently based on 14.04.3 Release 20150728
   mappings(:region_to_empire_ami) do
-    set!('us-east-1'.disable_camel!, :ami => 'ami-03ee1c15')
-    set!('us-east-2'.disable_camel!, :ami => 'ami-c82b0ead')
-    set!('us-west-1'.disable_camel!, :ami => 'ami-6e164b0e')
-    set!('us-west-2'.disable_camel!, :ami => 'ami-e1289181')
+    set!('us-east-1'.disable_camel!, :ami => 'ami-1e1c0009')
+    set!('us-east-2'.disable_camel!, :ami => 'ami-4cf5af29')
+    set!('us-west-1'.disable_camel!, :ami => 'ami-2fecbd4f')
+    set!('us-west-2'.disable_camel!, :ami => 'ami-15d66775')
   end
 end
