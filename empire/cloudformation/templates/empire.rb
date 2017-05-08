87311c77aa6d3c57296a6f8cb8a272623b685a8a
Greg Swallow <gswallow@gmail.com>
2017-05-08 12:23:24 -0400

    Bump number of controllers.

--- empire/cloudformation/templates/empire.rb
+++ empire/cloudformation/templates/empire.rb
@@ -284,8 +284,7 @@ EOF
   dynamic!(:auto_scaling_group,
            'controller',
            :launch_config => :controller_launch_config,
-           :desired_capacity => 2,
-           :max_size => 2,
+           :desired_capacity => 3,
            :subnets => lookup.get_subnets(vpc),
            :notification_topic => 'EmpireSnsNotificationTopic')
 
