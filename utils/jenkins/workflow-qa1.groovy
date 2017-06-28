d8a37aabb6e0560b5a051907dc520c0ede0d08ea
Greg Swallow <gswallow@gmail.com>
2017-06-28 12:54:34 -0400

    Sigh.

--- utils/jenkins/workflow-qa1.groovy
+++ utils/jenkins/workflow-qa1.groovy
@@ -108,6 +108,17 @@ parallel first: {
     [$class: 'StringParameterValue', name: 'max_size', value: '4'],
     [$class: 'StringParameterValue', name: 'desired_capacity', value: '4']
   ]
+}, firstpointfive: {
+  build job: '531-launch-slowconvert',
+  parameters: [
+    [$class: 'TextParameterValue', name: 'environment', value: workflow_env],
+    [$class: 'TextParameterValue', name: 'region', value: workflow_aws_region],
+    [$class: 'CredentialsParameterValue', description: '', name: 'workflow_aws_access_key_id', value: workflow_aws_access_key_id],
+    [$class: 'CredentialsParameterValue', description: '', name: 'workflow_aws_secret_access_key', value: workflow_aws_secret_access_key],
+    [$class: 'StringParameterValue', name: 'instance_type', value: 't2.medium'],
+    [$class: 'StringParameterValue', name: 'max_size', value: '4'],
+    [$class: 'StringParameterValue', name: 'desired_capacity', value: '4']
+  ]
 }, second: {
   build job: '600-launch-nginx',
   parameters: [
