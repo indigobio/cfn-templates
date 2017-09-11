a4ce4199ce1c5e627f7ee9e39403f880f9004744
Greg Swallow <gswallow@gmail.com>
2017-09-11 17:42:33 -0400

    Add missing AMI for us-east-2 (will probably never be used but hey.  Uniformity.)

--- stacks/cloudformation/components/win2k8_ami.rb
+++ stacks/cloudformation/components/win2k8_ami.rb
@@ -2,6 +2,7 @@ SparkleFormation.build do
   mappings(:region_to_win2k8_ami) do
     # 	Windows_Server-2008-R2_SP1-English-64Bit-Base
     set!('us-east-1'.disable_camel!, :ami => 'ami-8c7b2a9b')
+    set!('us-east-2'.disable_camel!, :ami => 'ami-863f1de3')
     set!('us-west-1'.disable_camel!, :ami => 'ami-acd890cc')
     set!('us-west-2'.disable_camel!, :ami => 'ami-d49035b4')
   end
