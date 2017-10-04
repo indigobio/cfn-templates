b72a75ea53a3890580a760524b6affb2469b830e
Greg Swallow <gswallow@gmail.com>
2017-10-04 13:57:30 -0400

    Add connection draining policies

--- vpc/cloudformation/dynamics/elb.rb
+++ vpc/cloudformation/dynamics/elb.rb
@@ -37,6 +37,21 @@ SparkleFormation.dynamic(:elb) do |_name, _config = {}|
     constraint_description 'can only contain ASCII characters'
   end
 
+  parameters("#{_name}_lb_draining_enabled".to_sym) do
+    type 'String'
+    allowed_values %w(true false)
+    default _config.fetch(:draining_policy, 'true')
+    description "Enable connection draining"
+  end
+
+  parameters("#{_name}_lb_draining_timeout".to_sym) do
+    type 'Number'
+    max_value 3600
+    min_value 1
+    default _config.fetch(:draining_timeout, 300)
+    description "Connection drain timeout.  Use a value between 1 and 3600."
+  end
+
   resources("#{_name.gsub('-','_')}_elb".to_sym) do
     type 'AWS::ElasticLoadBalancing::LoadBalancer'
     depends_on _array( 'VpcIgwAttachment' )
@@ -65,6 +80,10 @@ SparkleFormation.dynamic(:elb) do |_name, _config = {}|
         target "TCP:#{_config[:listeners].first[:instance_port]}"
         timeout "5"
         unhealthy_threshold "3"
+      end
+      connection_draining_policy do
+        enabled ref!("#{_name}_lb_draining_enabled".to_sym)
+        timeout ref!("#{_name}_lb_draining_timeout".to_sym)
       end
       policies _array(
         *_config[:policies].map { |l| -> {
