8f79bdf2e578667c953de91e31ad14514cf3e9aa
Greg Swallow <gswallow@gmail.com>
2017-10-09 16:07:42 -0400

    Update base ami

--- stacks/cloudformation/components/win2k8_ami.rb
+++ stacks/cloudformation/components/win2k8_ami.rb
@@ -1,9 +1,9 @@
 SparkleFormation.build do
   mappings(:region_to_win2k8_ami) do
     # 	Windows_Server-2008-R2_SP1-English-64Bit-Base
-    set!('us-east-1'.disable_camel!, :ami => 'ami-d86797a2')
-    set!('us-east-2'.disable_camel!, :ami => 'ami-335a7756')
-    set!('us-west-1'.disable_camel!, :ami => 'ami-40625220')
-    set!('us-west-2'.disable_camel!, :ami => 'ami-d9ef16a1')
+    set!('us-east-1'.disable_camel!, :ami => 'ami-405c913a')
+    set!('us-east-2'.disable_camel!, :ami => 'ami-a18ba6c4')
+    set!('us-west-1'.disable_camel!, :ami => 'ami-5acdfe3a')
+    set!('us-west-2'.disable_camel!, :ami => 'ami-86a96efe')
   end
 end
