bb84db1435c6d057ca897f6cedb47ac3ad232359
Greg Swallow <gswallow@gmail.com>
2017-10-09 14:34:01 -0400

    Add an assets.X.net CNAME that points to cloudformation.

--- stacks/cloudformation/templates/cloudfront.rb
+++ stacks/cloudformation/templates/cloudfront.rb
@@ -2,11 +2,20 @@ require 'sparkle_formation'
 require_relative '../../../utils/environment'
 require_relative '../../../utils/lookup'
 
-SparkleFormation.new('cloudfront')load(:git_rev_outputs).overrides do
+SparkleFormation.new('cloudfront').load(:git_rev_outputs).overrides do
   set!('AWSTemplateFormatVersion', '2010-09-09')
   description <<EOF
 Creates a Cloudfront distribution and an S3 bucket to hold public assets.
 EOF
 
   dynamic!(:cloudfront_distribution, 'assets', :origin => "static.#{ENV['public_domain']}")
+
+  dynamic!(:route53_record_set, 'assets',
+           :type =>  'CNAME',
+           :record => 'assets',
+           :target => :assets_cloudfront_distribution,
+           :domain_name => ENV['public_domain'],
+           :attr => 'DomainName',
+           :ttl => '60'
+           )
 end
